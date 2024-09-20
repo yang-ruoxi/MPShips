740e5dee9a72bd9c54e9e0ae020baca66e192807
yang-ruoxi <roxie@Live.cn>
2024-09-20 13:42:52 -0700

    more update

--- mpships/src/mpships/__init__.py
+++ mpships/src/mpships/__init__.py
@@ -5,7 +5,8 @@ __email__ = 'yangroxie@gmail.com'
 __version__ = '0.1.0'
 
 from pathlib import Path
-from mpships.materials_graph import MaterialsGraphAIO
+from mpships.materials_graph.materials_graph import MaterialsGraphAIO
+from mpships.ELATE_Crystal.elate_dash import ELATE
 
 
-MODULE_PATH = Path(__file__).parents[0]
+MPSHIPS_MODULE_PATH = Path(__file__).parents[0]
